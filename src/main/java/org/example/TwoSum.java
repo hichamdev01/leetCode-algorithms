c8dfe5d2ead33520b4a3a4b8f8b7b82ccad501e4
Hicham Labani <hichamlabani@MacBookPro.lan>
2025-09-26 23:23:10 +0100

    TwoSum algo

--- src/main/java/org/example/TwoSum.java
+++ src/main/java/org/example/TwoSum.java
@@ -5,15 +5,17 @@ import java.util.Arrays;
 public class TwoSum {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 5, 4}, 7)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,3}, 6)));
     }
 
     private static int[] twoSum(int[] nums, int target) {
-        for (int i = 0 ; i < nums.length-1 ; i++){
-            if(nums[i] + nums[i+1] == target){
-                return new int[]{i , i+1};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
-        return new int[]{}; // if no solution found
+        return new int[]{}; // no solution found
     }
 }
